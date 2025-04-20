48d6749e8a85dc29e709e6fa6d84382a06e80373
Ezequiel Binker <ezebinker@gmail.com>
2025-04-20 16:46:40 -0300

    feat: add image classification pipeline example

---
+++ 07_pipeline_image_classification.py
@@ -0,0 +1,16 @@
+# Import the pipeline function from the transformers library
+from transformers import pipeline 
+
+# Load the image classification pipeline
+classifier = pipeline(task="image-classification")
+
+# Classify an image
+preds = classifier(
+    "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2025/04/20/17451314384383.jpg"
+)
+
+# Round the scores to 4 decimal places and keep the label for each prediction
+preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in preds]
+
+# Print the predictions
+print(preds)
